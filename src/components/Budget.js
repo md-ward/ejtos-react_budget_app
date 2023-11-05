86110867ba654dad47bd406d05e5d4a4e4b52be9
md-ward <hp16806@gmail.com>
2023-11-05 03:56:38 -0500

    new changes

--- src/components/Budget.js
+++ src/components/Budget.js
@@ -1,17 +1,18 @@
 
-import React, { useContext, useState } from 'react';
+import React, { useContext } from 'react';
 import { AppContext } from '../context/AppContext';
 
 const Budget = () => {
-    const { budget } = useContext(AppContext);
-    const [newBudget, setNewBudget] = useState(budget);
+    const { budget,dispatch } = useContext(AppContext);
+    // const [newBudget, setNewBudget] = useState(budget);
     const handleBudgetChange = (event) => {
-        setNewBudget(event.target.value);
+        // setNewBudget(event.target.value);
+        dispatch({ type: 'SET_BUDGET', payload: event.target.value });
     }
     return (
 <div className='alert alert-secondary'>
 <span>Budget: £{budget}</span>
-<input type="number" step="10" value={newBudget} onChange={handleBudgetChange}></input>
+<input type="number" step="10" max={20000} value={budget} onChange={handleBudgetChange}></input>
 </div>
     );
 };
